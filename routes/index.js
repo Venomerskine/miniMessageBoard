9413d71ccc68e7e303df2cdc3cca06a962ff51bc
Lincoln Kubasu <kubasulincoln@gmail.com>
2026-01-29 08:25:04 +0300

    added router for delete

--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,13 @@ const express =  require('express')
 const router =  express.Router()
 const messageController = require("../controllers/messageControleers")
 
-// const messages = [
-//   {
-//     text: "Hi there!",
-//     user: "Amando",
-//     added: new Date()
-//   },
-//   {
-//     text: "Hello World!",
-//     user: "Charles",
-//     added: new Date()
-//   }
-// ];
-
 
 router.get("/", messageController.getMessages)
 router.post("/new", messageController.addMessage)
 
 router.get("/message/:id", messageController.showmessageDetails);
+router.post("/message/:id/delete", messageController.deleteMessage);
+
 
 
 
