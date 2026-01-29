a649b60d0c94acbdb81c230abfb33dd20bafe440
Lincoln Kubasu <kubasulincoln@gmail.com>
2026-01-29 15:38:07 +0300

    pool

--- db/pool.js
+++ db/pool.js
@@ -1,7 +1,4 @@
-require("dotenv").config();
 const { Pool } = require("pg");
-
-console.log("DATABASE_URL:", process.env.DATABASE_URL)
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -9,10 +6,6 @@ const pool = new Pool({
     process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
       : false,
-});
-
-pool.on("connect", () => {
-  console.log("Connected to PostgreSQL");
 });
 
 module.exports = pool;
